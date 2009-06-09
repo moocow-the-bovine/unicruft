4d68f1067fda9d042c35d9c3bf4721b7faf60ed5
Bryan Jurish <jurish@bbaw.de>
2009-06-09 10:46:43 +0000

    + documented Unicruft.pm
    git-svn-id: file:///home/svn/dev/unicruft/trunk/perl@2688 4a515de8-beb3-11dc-84d6-2b654028afac

--- Unicruft.pm
+++ Unicruft.pm
@@ -78,11 +78,94 @@ Unicruft - Perl interface to the unicruft transliteration library
 
   use Unicruft;
 
-  ##... stuff happens
+  $libversion = Unicruft::library_version();
+
+  $u8str = Unicruft::latin1_to_utf8($l1str);
+  $astr  = Unicruft::utf8_to_ascii($u8str);
+  $l1str = Unicruft::utf8_to_latin1($u8str);
+  $l1str = Unicruft::utf8_to_latin1_de($u8str);
 
 =head1 DESCRIPTION
 
-Not yet written.
+The perl Unicruft package provides a perl interface to the
+libunicruft library, which is itself derived in part from
+the Text::Unidecode perl module.
+
+=head2 FUNCTIONS
+
+=head3 library_version
+
+Returns the version string of the unicruft C library against which
+this perl module was compiled.
+
+=head3 latin1_to_utf8
+
+ $u8str = Unicruft::latin1_to_utf8($l1str);
+
+Converts the Latin-1 (ISO-8859-1) string $l1str to UTF-8.
+This task is better accomplished either with perl's utf8::upgrade() function
+or the perl Encode module; it is included here only for completeness' sake.
+
+$l1str may be either a byte-string or a perl-native UTF-8 string (i.e. a scalar with the SvUTF8 flag set).
+The returned string $u8str will have its UTF-8 flag set.
+
+=head3 utf8_to_ascii
+
+ $astr  = Unicruft::utf8_to_ascii($u8str);
+
+Approximate the UTF-8 string $u8str as 7-bit ASCII.
+This is basically just a (fast) re-implementation of Text::Unidecode::unidecode($u8str).
+
+$u8str may be either a byte-string (assumed to contain a valid UTF-8 byte sequence)
+or a perl-native UTF-8 string (i.e. a scalar with the SvUTF8 flag set).
+The returned string $astr will have its UTF-8 flag cleared
+(although this is pretty arbitrary here, since 7-bit ASCII is also valid UTF-8).
+
+=head3 utf8_to_latin1
+
+ $l1str = Unicruft::utf8_to_latin1($u8str);
+
+Approximate the UTF-8 string $u8str as 8-bit Latin-1 (ISO-8859-1).
+
+$u8str may be either a byte-string (assumed to contain a valid UTF-8 byte sequence)
+or a perl-native UTF-8 string (i.e. a scalar with the SvUTF8 flag set).
+The returned string $l1str will have its UTF-8 flag cleared.
+
+=head3 utf8_to_latin1_de
+
+ $l1str = Unicruft::utf8_to_latin1_de($u8str);
+
+Approximate the UTF-8 string $u8str as 8-bit Latin-1 (ISO-8859-1) using only
+characters which occur in contemporary German orthography.
+
+$u8str may be either a byte-string (assumed to contain a valid UTF-8 byte sequence)
+or a perl-native UTF-8 string (i.e. a scalar with the SvUTF8 flag set).
+The returned string $l1str will have its UTF-8 flag cleared.
+
+=head2 LOW-LEVEL FUNCTIONS
+
+For each conversion function C<X_to_Y>, there is an underlying
+C<ux_X_to_Y> function which places stricter requirements on its
+argument string (potentially downgrading it to a byte-string),
+but which is slightly faster since no copying or perl-level
+conditionals are required.
+
+=head3 ux_latin1_to_utf8
+
+Like L<latin1_to_utf8>(), but requires its argument to be a Latin-1-encoded byte string.
+
+=head3 ux_utf8_to_ascii
+
+Like L<utf8_to_ascii>(), but requires its argument to be a UTF-8-encoded byte string.
+
+=head3 ux_utf8_to_latin1
+
+Like L<utf8_to_latin1>(), but requires its argument to be a UTF-8-encoded byte string.
+
+=head3 ux_utf8_to_latin1_de
+
+Like L<utf8_to_latin1_de>(), but requires its argument to be a UTF-8-encoded byte string.
+
 
 =head1 SEE ALSO
 
