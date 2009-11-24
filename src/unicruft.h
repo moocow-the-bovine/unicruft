45b6c9c05d61a8acf4839d84ee5f571f931148e7
Bryan Jurish <jurish@bbaw.de>
2009-11-24 13:36:33 +0000

    + added '#ifdef __cplusplus' wrapper code to unicruft.h
    git-svn-id: file:///home/svn/dev/unicruft/trunk@3361 4a515de8-beb3-11dc-84d6-2b654028afac

--- src/unicruft.h
+++ src/unicruft.h
@@ -14,11 +14,19 @@
 #ifndef _UNICRUFT_H
 #define _UNICRUFT_H
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 /*#include <uxUtf8.h>*/   /*-- available, but we shouldn't include it by default --*/
 /*#include <uxConfig.h>*/ /*-- this gets read by uxCommon.h --*/
 #include <uxCommon.h>
 /*#include <uxBuffer.h>*/ /*-- this gets read by uxUnidecode.h --*/
 #include <uxUnidecode.h>
 #include <uxDEpp.h>
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif /* _UNICRUFT_H */
